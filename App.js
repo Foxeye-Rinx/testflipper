d47aab935e9231002ac844018ac23f292cbc2935
FoxeyeRinx <foxeye.rinx@gmail.com>
2023-05-28 11:30:09 +0700

    npx expo install expo-constants
    fixup
    
    
    Delete App.json

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+const apiUrl = Constants.expoConfig.extra.apiUrl;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{apiUrl}</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ app.config.js
@@ -0,0 +1,38 @@
+export default {
+    "expo": {
+      "name": "TestFlipper",
+      "slug": "TestFlipper",
+      "version": "1.0.0",
+      "orientation": "portrait",
+      "icon": "./assets/icon.png",
+      "userInterfaceStyle": "light",
+      "splash": {
+        "image": "./assets/splash.png",
+        "resizeMode": "contain",
+        "backgroundColor": "#ffffff"
+      },
+      "assetBundlePatterns": [
+        "**/*"
+      ],
+      "ios": {
+        "supportsTablet": true
+      },
+      "android": {
+        package: "com.test.flipper",
+        versionCode: 1,
+        "adaptiveIcon": {
+          "foregroundImage": "./assets/adaptive-icon.png",
+          "backgroundColor": "#ffffff"
+        }
+      },
+      "web": {
+        "favicon": "./assets/favicon.png"
+      },
+      "extra": {
+        apiUrl: process.env.API_URL,
+        "eas": {
+          "projectId": "2374f634-c409-42fb-8c3b-c3d4d4bac9ca"
+        }
+      }
+    }
+  }
